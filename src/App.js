a21ee4ea3b70f13dce93c6aec7ae184482cab743
svanessar <sinreal420@gmail.com>
2022-11-15 08:59:08 -0500

    holii

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+const App=()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const bootcamps=[
+        {id:1,
+        name:'PHP Bootcamps',
+        average_rating:8},
+        {
+            id:1,
+            name:'Javascript Bootcamps',
+            average_rating:10}
+        
+    ]
+ return (
+  <div>
+    <h1>Este el componente app uwu</h1>
+     <ul>
+        { bootcamps.map((bootcamp, index)=>(
+            <li key={index}>{bootcamp.name}</li>
+        ))}
+     </ul>
+  </div>
+ )
 }
-
-export default App;
+export default App
